4da2578ade7c6fb3edd852c8c0f8993aa6e1b7b2
Alejandro Parra <alejandroparral@gmail.com>
2025-09-11 11:25:48 -0500

    Update QuoteCreditVariable.kt

--- app/src/main/java/co/japl/android/myapplication/finanzas/controller/simulators/variable/QuoteCreditVariable.kt
+++ app/src/main/java/co/japl/android/myapplication/finanzas/controller/simulators/variable/QuoteCreditVariable.kt
@@ -14,10 +14,12 @@ import co.com.japl.module.creditcard.views.simulator.Simulator
 import co.com.japl.ui.theme.MaterialThemeComposeUI
 import co.japl.android.myapplication.databinding.QuoteCreditVariableBinding
 import dagger.hilt.android.AndroidEntryPoint
+import co.com.japl.finances.iports.inbounds.creditcard.ISimulatorCreditVariablePort
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class QuoteCreditVariable : Fragment(){
-
+    @Inject lateinit var quoteCreditScv: ISimulatorCreditVariablePort
     val viewModel : FormViewModel by viewModels{
         ViewModelFactory(
             owner = this,
