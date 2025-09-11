e7a581dc99b935ea2764bc9c1fe8d8d14aa7acd7
Alejandro Parra <alejandroparral@gmail.com>
2025-09-11 10:50:02 -0500

    Update ProjectionFormViewModel.kt

--- japl-android-finances-paid-module/src/main/java/co/com/japl/module/paid/controllers/projections/forms/ProjectionFormViewModel.kt
+++ japl-android-finances-paid-module/src/main/java/co/com/japl/module/paid/controllers/projections/forms/ProjectionFormViewModel.kt
@@ -15,7 +15,6 @@ import co.com.japl.module.paid.R
 import co.com.japl.ui.utils.DateUtils
 import co.japl.android.graphs.utils.NumbersUtil
 import co.com.japl.ui.utils.initialFieldState
-import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
@@ -24,7 +23,6 @@ import java.math.BigDecimal
 import java.time.LocalDate
 import javax.inject.Inject
 
-@HiltViewModel
 class ProjectionFormViewModel @Inject constructor(
     @ApplicationContext private val context: Context,
     private val saveStateHandler: SavedStateHandle,
